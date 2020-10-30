6b08f57a83ff8362999377bfc89afb7df17ac4c3
Søren Roesgaard Nielsen <srd@danskecommodities.com>
2020-10-30 13:31:59 +0100

    Added Lecture 2

---
+++ Lecture2/s01e02_exercises.py
@@ -0,0 +1,5 @@
+# Exercise 1: https://codingdojo.org/kata/FooBarQix/
+# Exercise 2: https://codingdojo.org/kata/LeapYears/
+# Exercise 3: https://codingdojo.org/kata/Bowling/
+# Exercise 4: https://codingdojo.org/kata/DictionaryReplacer/
+# Exercise 5: https://codingdojo.org/kata/Diamond/
